0b75a3a80669378ec35cac192d6ba2b90349220b
shy159 <361802334@qq.com>
2018-07-09 09:12:11 +0800

    加了空格

--- src/main/java/com/itdreamworks/datamanage/config/shiro/ShiroRealm.java
+++ src/main/java/com/itdreamworks/datamanage/config/shiro/ShiroRealm.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class ShiroRealm extends AuthorizingRealm {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.  getLogger(this.getClass());
 
     @Autowired
     private EmployeeMapper employeeMapper;
