05e1898bc3bab955547a06f0396dcc6275714ac0
shy159 <361802334@qq.com>
2018-07-10 13:48:02 +0800

    百度地图查询

--- src/main/java/com/itdreamworks/datamanage/mapper/BoilerMapper.java
+++ src/main/java/com/itdreamworks/datamanage/mapper/BoilerMapper.java
@@ -12,13 +12,13 @@ public interface BoilerMapper {
     @ResultType(Boiler_Device_View.class)
     List<Boiler_Device_View>  getBoilerList(@Param("customerId") String customerId ,@Param("from") Integer from ,@Param("pageSize") Integer pageSize);
     @Insert("INSERT into Boiler(customerId,BoilerNo,DeviceNo,BolierType,\n" +
-            "BoilerTon,BoilerMedium,BoilerFuel,Status) VALUES(#{customerId},#{boilerNo},#{deviceNo},#{boilerType},\n" +
-            "#{boilerTon},#{boilerMedium},#{boilerFuel},#{status})")
+            "BoilerTon,BoilerMedium,BoilerFuel,Status,SaleTime) VALUES(#{customerId},#{boilerNo},#{deviceNo},#{boilerType},\n" +
+            "#{boilerTon},#{boilerMedium},#{boilerFuel},#{status},#{saleTime})")
     void create(Boiler_Device_View boiler_device_view);
     @Insert("insert into Location(deviceNo,lng,lat) values(#{deviceNo},#{lng},#{lat})")
     void insertLocation(Boiler_Device_View boiler_device_view);
     @Update("update Boiler set customerId=#{customerId},BoilerNo=#{boilerNo},DeviceNo=#{deviceNo},BolierType=#{boilerType},\n" +
-            "BoilerTon=#{boilerTon},BoilerMedium=#{boilerMedium},BoilerFuel=#{boilerFuel},Status=#{status})  ")
+            "BoilerTon=#{boilerTon},BoilerMedium=#{boilerMedium},BoilerFuel=#{boilerFuel},Status=#{status},SaleTime=#{saleTime})  ")
     void modify(Boiler_Device_View boiler_device_view);
     @Update("update Location set  deviceNo=#{deviceNo},lng=#{lng},lat=#{lat}")
     void modifyLocation(Boiler_Device_View boiler_device_view);
